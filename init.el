3fe041bb0ed4be2d64ba7718b2e0ba027c0c830b
Jonathan Petersson <jpetersson@redbridge.se>
2020-01-10 12:06:32 +0100

    initial commit

---
+++ init.el
@@ -0,0 +1,105 @@
+(require 'package)
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                 (not (gnutls-available-p))))
+    (proto (if no-ssl "http" "https")))
+    ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
+    (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+    ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+    (when (< emacs-major-version 24)
+    ;; For important compatibility libraries like cl-lib
+(add-to-list 'package-archives '("gnu" . (concat proto "://elpa.gnu.org/packages/")))))
+
+(package-initialize)
+
+
+;; We will use 'use-package' to install and configure packages.
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(eval-when-compile (require 'use-package))
+
+(setq use-package-always-ensure t)
+(use-package exec-path-from-shell
+  :ensure t)
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize))
+
+(global-font-lock-mode 0)
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (shell-pop git-gutter magit markdown-mode haml-mode yaml-mode visual-regexp use-package)))
+ '(shell-pop-shell-type
+   (quote
+    ("ansi-term" "*ansi-term*"
+     (lambda nil
+       (ansi-term shell-pop-term-shell)))))
+ '(shell-pop-universal-key "s-="))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(setq
+ font-lock-maximum-decoration '((c-mode . 1) (t . 3))
+ require-final-newline t
+ inhibit-startup-message t         ; Don't show the startup message...
+ inhibit-startup-screen t          ; ... or screen
+ cursor-in-non-selected-windows t  ; Hide the cursor in inactive windows
+
+ echo-keystrokes 0.1               ; Show keystrokes right away, don't show the message in the scratch buffer
+ initial-scratch-message nil       ; Empty scratch buffer
+ initial-major-mode 'org-mode      ; Org mode by default
+ sentence-end-double-space nil     ; Sentences should end in one space, come on!
+ help-window-select t              ; Select help window so it's easy to quit it with 'q'
+ tab-width 2
+ js-indent-level 2
+ css-indent-offset 2
+ c-basic-offset 2
+ )
+
+(setq-default indent-tabs-mode nil)
+(setq-default c-basic-offset 2)
+(setq-default tab-width 2)
+(setq-default c-basic-indent 2)
+
+(fset 'yes-or-no-p 'y-or-n-p)      ; y and n instead of yes and no everywhere else
+(global-unset-key (kbd "s-p"))     ; Don't print
+
+(use-package visual-regexp
+  :config
+  (define-key global-map (kbd "M-s-f") 'vr/replace)
+  (define-key global-map (kbd "s-r") 'vr/replace))  ;; Cmd+r find and replace
+
+(use-package yaml-mode)
+(use-package haml-mode)
+(use-package markdown-mode)
+
+(use-package magit
+  :config
+  (global-set-key (kbd "s-;") 'magit-status))   ;; Cmd+g for git status
+
+(use-package git-gutter
+  :diminish
+  :config
+  (global-git-gutter-mode 't)
+  (set-face-background 'git-gutter:modified 'nil)   ;; background color
+  (set-face-foreground 'git-gutter:added "green4")
+  (set-face-foreground 'git-gutter:deleted "red"))
+
+(use-package shell-pop
+  :config
+  (custom-set-variables
+   '(shell-pop-shell-type (quote ("ansi-term" "*ansi-term*" (lambda nil (ansi-term shell-pop-term-shell)))))
+   '(shell-pop-universal-key "s-=")))
+
+(global-display-line-numbers-mode)
+(show-paren-mode 1)
